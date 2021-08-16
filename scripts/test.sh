5f7cb4b7ad749100287dd48d163af58769c83c5d
vkoski-godaddy <81697983+vkoski-godaddy@users.noreply.github.com>
2021-08-16 00:04:30 -0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 joe=$(cat /etc/hostname)
 fred='this is a test'
-alex=$(awk -F' is a ' '{print $1}' <<< $fred)
-mike=$(awk -F' is a ' '{print $2}' <<< $fred)
+alex=$(awk -F' is a ' '{print $1}' <<< "$fred")
+mike=$(awk -F' is a ' '{print $2}' <<< "$fred")
 ruben="$mike $alex $fred $joe"
 echo "$ruben"
